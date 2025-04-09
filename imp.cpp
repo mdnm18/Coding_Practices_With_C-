69d6e9c9b5352cba6d353b8479e3f6dea2407659
MD NAYAJ MONDAL <md.n.m.pius18@gmail.com>
2025-04-09 19:24:12 +0530

    part-2

---
+++ imp.cpp
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include <unistd.h>
+int main()
+{
+	char a;
+	printf("TO AISA HAI KI :- \n");
+		printf("PRESS 'ENTER'...................................");
+		scanf("%c",&a);
+	system("cls");
+	printf("Mujhe na tumse kurti si muhabbat nahi, saree sa ishq chahiye.. Nahi samjhi?\n");
+		printf("PRESS 'ENTER'...................................");
+		scanf("%c",&a);
+	system("cls");
+	printf("Mujhe, jaldabazi mein pehna hua nishchay nai, itminan se bandha hua ishq chahiye..\n");
+		printf("PRESS 'ENTER'...................................");
+		scanf("%c",&a);
+	system("cls");
+	printf("Mujhe nazro ke saamne milne vala nahi, almari mein kahi sambhaal ke rakha hua ishq chahiye..\n");
+		printf("PRESS 'ENTER'...................................");
+		scanf("%c",&a);
+	system("cls");
+	printf("Mujhe tumhare dosto ki tareefo vala nahi, Par tumhari maa ki chupi kahaniyon vala ishq chahiye..\n");
+		printf("PRESS 'ENTER'...................................");
+		scanf("%c",&a);
+	system("cls");
+	printf("Mujhe kurti ke nayepan si sahuliyat nahi, Par saree ka saalon ka thehraav aur gehrapan chahiye..\n");
+		printf("PRESS 'ENTER'...................................");
+		scanf("%c",&a);
+	system("cls");
+	printf("Mujhe aasani se sulajhane vala nahi, Balki, parat dar parat ulajhti paheli vala ishq chahiye..\n");
+		printf("PRESS 'ENTER'...................................");
+		scanf("%c",&a);
+	system("cls");
+	printf("Aur Mujhe saralta se hothon ko chune vala nahi, balki, kamar pe haq se haath rakhne vala ishq chahiye..\n");
+		printf("PRESS 'ENTER'...................................");
+		scanf("%c",&a);
+	system("cls");
+	int i;
+	printf("I WANT TO WALK WITH YOU AS YOUR SHADOW FOR THE REST OF MY LIFE \n");
+    for (i = 1; i <= 3; i++)
+	{
+        printf("WOULD YOU LIKE TO BE MY COMPANION IN MY RUN CHASE ?\n");
+        printf("WILL YOU BE MINE FOREVER ?\n");
+        fflush(stdout); 
+        sleep(1);
+	}
+	return 0;
+}
+
+
+
+
+
+
+/*
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+								JUST PRESS 'F11' KEY IN THE KEYBOARD															'
+
+
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+*/
+
+
+
+
+
+
+
+
+
